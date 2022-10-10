b67995bc826435d82f17c31430db330e1965c378
汪永晖 <wangyonghui@guokang.com>
2022-10-10 10:35:49 +0800

    Initial commit

---
+++ src/main/java/top/alexmmd/dog/DogHoleApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd.dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogHoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogHoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/alexmmd/dog/DogHoleApplicationTests.java
@@ -0,0 +1,13 @@
+package top.alexmmd.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogHoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
