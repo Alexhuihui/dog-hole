16911947620d8a7372106a794a33bd01ac511fda
汪永晖 <wangyonghui@guokang.com>
2023-01-05 17:52:16 +0800

    feat: 调试数据库动态配置

--- src/main/java/top/alexmmd/dog/service/impl/LoginAccountServiceImpl.java
+++ src/main/java/top/alexmmd/dog/service/impl/LoginAccountServiceImpl.java
@@ -1,6 +1,9 @@
 package top.alexmmd.dog.service.impl;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import top.alexmmd.dog.dao.LoginAccountDao;
 import top.alexmmd.dog.domain.entity.LoginAccount;
@@ -13,10 +16,23 @@ import top.alexmmd.dog.service.ILoginAccountService;
  * @since 2022-10-11 09:02:02
  */
 @Service
+@Slf4j
 public class LoginAccountServiceImpl implements ILoginAccountService {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Resource
     private LoginAccountDao loginAccountDao;
+
+    @PostConstruct
+    public void init() {
+        log.info("数据库{}", url);
+        log.info(password);
+    }
 
 
     @Override
